09934eff2f475adaa4e9ab7f558fb4f2f58bcf06
Luiz Felippe <lfelippe570@gmail.com>
2025-07-03 23:00:12 -0300

    deploy teste

--- backend/controllers/transactionsController.ts
+++ backend/controllers/transactionsController.ts
@@ -35,12 +35,15 @@ const createTransaction = async (req: Request, res: Response) => {
     }
 
     const query = `
-      INSERT INTO transactions (title, amount, type, date, department, user_id)
-      VALUES ($1, $2, $3, $4, $5, $6)
+      INSERT INTO transactions (title, amount, type, date, department)
+      VALUES ($1, $2, $3, $4, $5)
       RETURNING *;
     `;
-    
+    console.log("Query SQL que será executada:", query); // Adicione esta linha
     const values = [title, parsedAmount, type, date, department || null];
+    
+    console.log("Valores que serão passados para a query:", values); // Adicione esta linha
+
     const result = await pool.query(query, values);
 
     res.status(201).json(result.rows[0]);
