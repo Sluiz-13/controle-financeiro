bd5cefd83ac316bf84cafb032e15b1599a814833
Luiz Felippe <lfelippe570@gmail.com>
2025-07-19 14:06:10 -0300

    Revert "feat: Atualiza configuração do banco de dados para usar connectionString"
    This reverts commit d06a826561c8fd4ab55f97ebc05c0d9999a96bc1.

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -2,7 +2,11 @@ import { Pool } from 'pg';
 import 'dotenv/config';
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432'), // Default to 5432 if not set
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 export default pool;
